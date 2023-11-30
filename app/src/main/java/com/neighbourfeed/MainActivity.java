feceb82e025c7c4c226be9e9d6fafc0373dbbbc4
Haydar Taha Tunç <tahatunc163@gmail.com>
2023-12-01 00:39:00 +0300

    ic_profile.xml, ic_filter.xml, ic_post.xml added on the project and tested for both dark and light theme

--- app/src/main/java/com/neighbourfeed/MainActivity.java
+++ app/src/main/java/com/neighbourfeed/MainActivity.java
@@ -1,5 +1,43 @@
 package com.neighbourfeed;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        ImageButton profileButton = findViewById(R.id.profileButton);
+        ImageButton filterButton = findViewById(R.id.filterButton);
+        ImageButton createPostButton = findViewById(R.id.createPostButton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //openProfile();
+            }
+        });
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //openFilter();
+            }
+        });
+
+        createPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //openCreatePost();
+            }
+        });
+    }
 }
