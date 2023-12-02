4f8c8f73df0971f15fb0bac68a84635b8935124e
Haydar Taha Tunç <tahatunc163@gmail.com>
2023-12-02 16:31:28 +0300

    User Profile Updated

--- app/src/main/java/com/neighbourfeed/UserProfile.java
+++ app/src/main/java/com/neighbourfeed/UserProfile.java
@@ -2,18 +2,23 @@ package com.neighbourfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import java.util.Objects;
 
 public class UserProfile extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +26,26 @@ public class UserProfile extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         MaterialButtonToggleGroup toggleGroup = findViewById(R.id.toggleGroup);
-        TextView textViewDisplay = findViewById(R.id.textViewDisplay);
+        LinearLayout linearLayoutPosts = findViewById(R.id.linearLayoutPosts);
 
         toggleGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+            //Create a TextView object and display the selected button's text in the LinearLayout
             if (isChecked) {
+                TextView textView = new TextView(getApplicationContext());
                 if (checkedId == R.id.btnPosts) {
-                    textViewDisplay.setText("Posts should be here");
+                    textView.setText("Posts");
                 } else if (checkedId == R.id.btnComments) {
-                    textViewDisplay.setText("Comments should be here");
+                    textView.setText("Comments");
+                } else if (checkedId == R.id.btnUpVotes) {
+                    textView.setText("Up Votes");
+                } else if (checkedId == R.id.btnDownVotes) {
+                    textView.setText("Down Votes");
                 }
+                //Add the TextView to the LinearLayout if the linearLayoutPosts is not empty then remove all the views
+                if (linearLayoutPosts.getChildCount() > 0) {
+                    linearLayoutPosts.removeAllViews();
+                }
+                linearLayoutPosts.addView(textView);
             }
         });
     }
